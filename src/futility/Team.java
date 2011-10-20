b8e5a2f1ff1b6a7a542456d5fa42dbfa9f006a81
Thomas Peterman <tpeteman@gmail.com>
2011-10-20 16:24:06 -0500

    Documentation changes. Please work.

--- src/futility/Team.java
+++ src/futility/Team.java
@@ -1,5 +1,15 @@
+/** @file Team.java
+ * Data representation of a player team.
+ * 
+ * @author Team F(utility)
+ * @date 20 October 2011
+ */
+
 package futility;
 
+/** @class Team
+ * The player agent's conception of a team, including the team's name and its side.
+ */
 public class Team {
     public String name = Settings.TEAM_NAME;
     public char side;
